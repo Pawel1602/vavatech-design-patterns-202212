554ff7e203c90e08c08b09aeb28687990d9c3c74
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-21 08:51:35 +0100

    Dodanie aplikacji konsolowej

---
+++ src/DesignPatterns/StatePattern/OrderStatusExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    internal static class OrderStatusExtensions
+    {
+        public static void Dump(this OrderStatus status)
+        {
+            Console.ForegroundColor = Create(status);
+            Console.WriteLine(status);
+            Console.ResetColor();
+        }
+
+        // Metoda wytwórcza (fabrykująca)
+        private static ConsoleColor Create(OrderStatus orderStatus) => orderStatus switch
+        {
+            OrderStatus.Pending => ConsoleColor.DarkYellow,
+            OrderStatus.Sent => ConsoleColor.Green,
+            OrderStatus.Delivered => ConsoleColor.Blue,
+            OrderStatus.Completed => ConsoleColor.Black,
+            OrderStatus.Cancelled => ConsoleColor.DarkGray,
+            _ => Console.ForegroundColor,
+        };
+    }
+}
---
+++ src/DesignPatterns/StatePattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                var order = new Order();
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(order);
+                Console.ResetColor();
+
+                order.Status.Dump();
+
+                while (!(order.Status == OrderStatus.Cancelled || order.Status == OrderStatus.Completed))
+                {
+                    if (order.CanConfirm)
+                        Console.Write("Confirm (Enter) ");
+
+                    if (order.CanCancel)
+                        Console.Write("(C)ancel ");
+
+                    var key = Console.ReadKey().Key;
+
+                    Console.WriteLine();
+
+                    if (key == ConsoleKey.Enter)
+                        order.Confirm();
+
+                    if (key == ConsoleKey.C)
+                        order.Cancel();
+
+                    order.Status.Dump();
+
+                }
+
+                Console.Clear();
+
+            }
+        }
+    }
+}
